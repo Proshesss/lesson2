2fc7f5bdfaf29dd17059a847d0e417841b59cf0c
Proshesss <djmbrovskiy@gmail.com>
2024-10-06 17:42:40 +0500

    Add files via upload

---
+++ modul1_2.py
@@ -0,0 +1,7 @@
+Home_work = 12
+print(Home_work)
+time = 1.5
+print(time)
+course =("Python")
+print(course)
+print("курс:",course,"всего задач:",Home_work,"затрачено времени:",time,"среднее время выполнения:",time / Home_work)
